8259e0ef4ce9eac5e194a46d5acc4c35b31963e7
aaljaish <47437590+aaljaish@users.noreply.github.com>
2025-08-15 23:59:02 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
+/** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#006a78',
+        secondary: '#1eaeba'
+      },
+      ringColor: {
+        primary: '#006a78',
+        secondary: '#1eaeba'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
